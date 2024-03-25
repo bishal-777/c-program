68cd01c66fe759c6fdcf1180baf8be2c9f35a308
bishal-777 <152830259+bishal-777@users.noreply.github.com>
2024-03-25 22:41:14 +0545

    Add files via upload

---
+++ swapingbycallbyreference/swapingbycallbyreference/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void swap(int*,int*);
+
+int main(void)
+{
+    int a,b;
+    printf("Enter any two numbers:");
+    scanf("%d%d",&a,&b);
+    printf("\nAfter swapping:a=%d and b=%d",a,b);
+    swap(&a,&b);
+    printf("\nAfter swapping:a=%d and b=%d",a,b);
+    return 0;
+}
+
+void swap(int*x,int*y)
+{
+    int c=*x;
+    *x=*y;
+    *y=c;
+ }
