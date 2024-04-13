98c9c14c985eab824062341bb38f78b7e848a73a
bishal-777 <152830259+bishal-777@users.noreply.github.com>
2024-04-13 23:20:09 +0545

    Add files via upload

---
+++ pattern_1.c
@@ -0,0 +1,24 @@
+/*This program prints following pattern:
+B
+BI
+BIS
+BISH
+BISHA
+BISHAL*/
+ 
+#include <stdio.h>
+
+int main(void)
+    {
+    char str[6]= "BISHAL";
+    int i,j;
+    for(i=0;str[i]!=0;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            putchar(str[j]);
+        }
+        printf("\n");
+    }
+    return 0;
+    }
